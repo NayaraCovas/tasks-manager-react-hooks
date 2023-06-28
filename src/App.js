471ea875ae71c6df20a518a7abf0ccf34baba58d
Nayara Covas <nayaracovas1@outlook.com>
2023-06-28 12:59:27 +0200

    fixed delete

--- src/App.js
+++ src/App.js
@@ -23,8 +23,7 @@ const App =() => {
   //Fetch Tasks
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
-    const data = await res.json ()
-    console.log(data)
+    const data = await res.json ()    
     return(data)
   }
   //Add Task
@@ -38,9 +37,12 @@ const App =() => {
 
   //Delete task
 
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) =>task.id !== id))
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
 
+    setTasks(tasks.filter((task) =>task.id !== id))
   }
 
   //Toggle Reminder
